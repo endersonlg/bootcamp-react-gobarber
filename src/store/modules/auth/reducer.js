74a95981ba7b42f3b0038d24156d59d2bb52e51f
endersonlg <endersonluciog@gmail.com>
2020-04-03 02:22:25 -0300

    Correcao no type da action auth

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -13,7 +13,7 @@ export default function auth(state = INITIAL_STATE, action) {
         draft.loading = true;
         break;
       }
-      case '@auth/SIGN_IN_SUCESS': {
+      case '@auth/SIGN_IN_SUCCESS': {
         draft.token = action.payload.token;
         draft.signed = true;
         draft.loading = false;
