1536d9f5d62a4122caba1ff7d4d4bd0b2a5108bf
endersonlg <endersonluciog@gmail.com>
2020-04-05 15:51:53 -0300

    27 - Logout da aplicação

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -59,8 +59,13 @@ export function setToken({ payload }) {
   }
 }
 
+export function signOut() {
+  history.push('/');
+}
+
 export default all([
   takeLatest('persist/REHYDRATE', setToken),
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
   takeLatest('@auth/SIGN_UP_REQUEST', singUp),
+  takeLatest('@auth/SIGN_OUT', signOut),
 ]);
